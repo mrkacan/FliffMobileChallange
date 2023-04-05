de2f4089b69a95cc3454e12cf6bbcf2a43242aa0
Emre Kaçan <>
2023-04-05 11:41:25 +0300

    - makeup - added hitSlop to GoBack component

--- src/components/GoBack.tsx
+++ src/components/GoBack.tsx
@@ -1,34 +1,50 @@
 import React from 'react';
 import styled from 'styled-components/native';
 import {useNavigation} from '@react-navigation/native';
+import {colors} from '../themes/colors';
 
-const StyledTouchableHighlight = styled.TouchableHighlight`
-  width: 25px;
-  height: 25px;
-  border-radius: 20px;
-  background-color: #000000;
-  justify-content: center;
+const StyledTouchableOpacity = styled.TouchableOpacity`
   align-items: center;
-  margin: 20px;
+  margin: 10px 20px 5px 20px;
+  flex-direction: row;
+  justify-content: flex-start;
 `;
 
 const StyledArrow = styled.View`
-  width: 10px;
-  height: 10px;
+  width: 9px;
+  height: 9px;
   border-top-width: 2px;
-  border-top-color: #ffffff;
+  border-top-color: ${colors.primary};
   border-right-width: 2px;
-  border-right-color: #ffffff;
+  border-right-color: ${colors.primary};
   transform: rotate(-135deg);
-  margin-right: -3px;
 `;
 
+const StyledText = styled.Text`
+  color: ${colors.primary};
+  font-size: 15px;
+  font-weight: 500;
+  margin-left: 5px;
+`;
+
+const hitSlop = {
+  top: 20,
+  bottom: 20,
+  left: 20,
+  right: 20,
+};
 const GoBack: React.FC = () => {
   const navigation = useNavigation();
   return (
-    <StyledTouchableHighlight onPress={navigation.goBack}>
-      <StyledArrow />
-    </StyledTouchableHighlight>
+    <StyledTouchableOpacity
+      onPress={navigation.goBack}
+      activeOpacity={0.8}
+      hitSlop={hitSlop}>
+      <>
+        <StyledArrow />
+        <StyledText>Back</StyledText>
+      </>
+    </StyledTouchableOpacity>
   );
 };
 
